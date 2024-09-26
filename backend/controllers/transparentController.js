c4f7c2d1a1bb8c0dfe2a71f970a858d06475ebdd
Ajis <ajisriyananta@gmail.com>
2024-09-26 10:22:10 +0700

    controller_transparansi

---
+++ backend/controllers/transparentController.js
@@ -0,0 +1,247 @@
+import { PrismaClient } from "@prisma/client";
+
+import jwt from "jsonwebtoken";
+import { validationResult } from "express-validator";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const prisma = new PrismaClient();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const getProdukHukumAdmin = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    // Cek keberadaan refresh token
+    if (!refreshToken) {
+      return res.status(401).json({ msg: "Token tidak ditemukan" });
+    }
+
+    // Temukan administrator berdasarkan refresh token
+    const administrator = await prisma.administrator.findUnique({
+      where: {
+        refresh_token: refreshToken,
+      },
+    });
+
+    // Cek apakah administrator ditemukan
+    if (!administrator) {
+      return res.status(401).json({ msg: "Pengguna tidak ditemukan" });
+    }
+
+    // Cek peran administrator
+    if (administrator.role !== "administrator") {
+      return res.status(403).json({ msg: "Akses ditolak" });
+    }
+
+    // Ambil data dari tabel ProdukHukum
+    const produkHukum = await prisma.produkHukum.findMany({
+      include: {
+        createdBy: true, // Jika Anda ingin menyertakan informasi tentang administrator yang membuat entri
+      },
+    });
+
+    // Cek jika tidak ada data
+    if (produkHukum.length === 0) {
+      return res.status(200).json({ produkHukum: [] });
+    }
+
+    // Kirimkan data produk hukum
+    res.status(200).json({ produkHukum });
+  } catch (error) {
+    console.error("Error saat mengambil data produk hukum untuk admin:", error);
+    res.status(500).json({ msg: "Terjadi kesalahan pada server" });
+  }
+};
+
+export const createProdukHukum = async (req, res) => {
+  // Validasi input
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, deskripsi, waktu } = req.body;
+  const file = req.file; // Mengambil file dari upload
+
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    // Cek keberadaan refresh token
+    if (!refreshToken) {
+      return res.status(401).json({ msg: "Token tidak ditemukan" });
+    }
+
+    // Dekode token untuk mendapatkan informasi administrator
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    const administrator = await prisma.administrator.findUnique({
+      where: { id: decoded.administratorId },
+    });
+
+    // Cek apakah administrator ditemukan dan memiliki akses yang sesuai
+    if (!administrator || administrator.role !== "administrator") {
+      return res.status(403).json({ msg: "Akses ditolak" });
+    }
+
+    // Cek apakah produk hukum dengan nama yang sama sudah ada
+    const existingProdukHukum = await prisma.produkHukum.findUnique({
+      where: { name: name },
+    });
+
+    if (existingProdukHukum) {
+      // Jika produk hukum sudah ada, kembalikan error
+      return res.status(400).json({
+        status: "fail",
+        errors: [
+          { msg: "Produk hukum sudah ada, tidak bisa membuat data baru" },
+        ],
+      });
+    } else {
+      // Buat produk hukum baru jika tidak ada yang sama
+      const newProdukHukum = await prisma.produkHukum.create({
+        data: {
+          name,
+          deskripsi,
+          waktu: new Date(waktu), // Pastikan waktu dikonversi ke tipe Date
+          file_url: file ? `/uploads/produk_hukum/${file.filename}` : null, // Mengatur URL file
+          createdbyId: administrator.id, // Menyimpan ID administrator yang membuat produk hukum
+        },
+      });
+
+      return res.status(201).json({
+        msg: "Produk hukum dibuat dengan sukses",
+        produkHukum: newProdukHukum,
+      });
+    }
+  } catch (error) {
+    console.error("Error saat membuat produk hukum:", error);
+    res.status(500).json({ msg: "Terjadi kesalahan pada server" });
+  }
+};
+
+export const updateProdukHukum = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { uuid, name, deskripsi, waktu } = req.body; // Menggunakan uuid
+  const file = req.file;
+
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res.status(401).json({ msg: "Token tidak ditemukan" });
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    const administrator = await prisma.administrator.findUnique({
+      where: { id: decoded.administratorId },
+    });
+
+    if (!administrator || administrator.role !== "administrator") {
+      return res.status(403).json({ msg: "Akses ditolak" });
+    }
+
+    const existingProdukHukum = await prisma.produkHukum.findUnique({
+      where: { uuid }, // Mencari berdasarkan uuid
+    });
+
+    if (!existingProdukHukum) {
+      return res.status(404).json({ msg: "Produk hukum tidak ditemukan" });
+    }
+
+    let filePathToDelete = null;
+    if (file && existingProdukHukum.file_url) {
+      filePathToDelete = path.join(
+        __dirname,
+        "..",
+        "uploads/produk_hukum",
+        path.basename(existingProdukHukum.file_url)
+      );
+    }
+
+    const updatedProdukHukum = await prisma.produkHukum.update({
+      where: { uuid }, // Mencari berdasarkan uuid
+      data: {
+        name,
+        deskripsi,
+        waktu: new Date(waktu),
+        file_url: file
+          ? `/uploads/produk_hukum/${file.filename}`
+          : existingProdukHukum.file_url,
+        updated_at: new Date(),
+      },
+    });
+
+    if (filePathToDelete && fs.existsSync(filePathToDelete)) {
+      fs.unlinkSync(filePathToDelete);
+      console.log(`Successfully deleted old file: ${filePathToDelete}`);
+    }
+
+    return res.status(200).json({
+      msg: "Produk hukum diperbarui dengan sukses",
+      produkHukum: updatedProdukHukum,
+    });
+  } catch (error) {
+    console.error("Error saat memperbarui produk hukum:", error);
+    res.status(500).json({ msg: "Terjadi kesalahan pada server" });
+  }
+};
+
+export const deleteProdukHukum = async (req, res) => {
+  const { uuid } = req.params; // Menggunakan uuid
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res.status(401).json({ msg: "Token tidak ditemukan" });
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    const administrator = await prisma.administrator.findUnique({
+      where: { id: decoded.administratorId },
+    });
+
+    if (!administrator || administrator.role !== "administrator") {
+      return res.status(403).json({ msg: "Akses ditolak" });
+    }
+
+    const existingProdukHukum = await prisma.produkHukum.findUnique({
+      where: { uuid }, // Mencari berdasarkan uuid
+    });
+
+    if (!existingProdukHukum) {
+      return res.status(404).json({ msg: "Produk hukum tidak ditemukan" });
+    }
+
+    // Delete file if it exists
+    const filePathToDelete = path.join(
+      __dirname,
+      "..",
+      "uploads/produk_hukum",
+      path.basename(existingProdukHukum.file_url)
+    );
+
+    if (fs.existsSync(filePathToDelete)) {
+      fs.unlinkSync(filePathToDelete);
+      console.log(`Successfully deleted file: ${filePathToDelete}`);
+    }
+
+    // Delete product hukum record
+    await prisma.produkHukum.delete({
+      where: { uuid }, // Mencari berdasarkan uuid
+    });
+
+    return res.status(200).json({ msg: "Produk hukum dihapus dengan sukses" });
+  } catch (error) {
+    console.error("Error saat menghapus produk hukum:", error);
+    res.status(500).json({ msg: "Terjadi kesalahan pada server" });
+  }
+};
